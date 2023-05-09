8554123596f8fdfd38be7fc6c660f2aa5cc3d053
andreivince <andreivince21@gmail.com>
2023-05-09 10:45:20 -0400

    Initial Commit

---
+++ GymFit/GymFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymFitApp.swift
+//  GymFit
+//
+//  Created by Andrei Goulart Mansilla Vince on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GymFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymFitUITests/GymFitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GymFitUITestsLaunchTests.swift
+//  GymFitUITests
+//
+//  Created by Andrei Goulart Mansilla Vince on 5/9/23.
+//
+
+import XCTest
+
+final class GymFitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
